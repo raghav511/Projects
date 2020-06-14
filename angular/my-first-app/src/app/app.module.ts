fb35a5b6d3846b4bdf66a954bf22b22bdb1ddb6d
ballur raghavendra raju <raghav0611@gmail.com>
2020-06-14 20:20:39 +0530

    14th jun 2020

--- angular/my-first-app/src/app/app.module.ts
+++ angular/my-first-app/src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { AppComponent } from './app.component';
-import {ServerComponent} from './app.server.'
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
